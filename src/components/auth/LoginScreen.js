33346e4d7e6caa708650ea3a1ec59153310359b7
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-06-09 11:16:54 -0300

    Validacion de login con Formik

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,46 +1,70 @@
 import React from "react";
+import { Formik } from "formik";
 import { useDispatch } from "react-redux";
 import { startLogin } from "../../actions/auth";
 
-import { useForm } from "../../hooks/useForm";
+// import { useForm } from "../../hooks/useForm";
 import "./login.css";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
 
-  const [formValues, handleInputChange] = useForm({
-    email: "challenge@alkemy.org",
-    password: "react",
-  });
-  const { email, password } = formValues;
-
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
-    dispatch(startLogin(email, password));
-  };
-
   return (
     <div className="d-flex justify-content-center">
-      <form className="box" onSubmit={handleSubmitForm}>
-        <h1>Login</h1>
-        <p className="text-muted"> Please enter your login and password!</p>
-        <input
-          autoComplete="off"
-          type="email"
-          name="email"
-          placeholder="Username"
-          value={email}
-          onChange={handleInputChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={handleInputChange}
-        />
-        <input type="submit" name="" value="Login" />
-      </form>
+      <Formik
+        initialValues={{ email: "challenge@alkemy.org", password: "react" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = "Este campo es requerido";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Ingrese un email correcto";
+          }
+          return errors;
+        }}
+        onSubmit={(values) => {
+          return dispatch(startLogin(values.email, values.password));
+        }}
+      >
+        {({
+          values,
+          touched,
+          errors,
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form className="box" onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <p className="text-muted"> Please enter your login and password!</p>
+            <input
+              autoComplete="off"
+              type="email"
+              name="email"
+              placeholder="Username"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={values.password}
+              onChange={handleChange}
+            />
+            <input
+              className="btn-login"
+              type="submit"
+              name=""
+              value="Login"
+              disabled={isSubmitting}
+            />
+          </form>
+        )}
+      </Formik>
     </div>
   );
 };
